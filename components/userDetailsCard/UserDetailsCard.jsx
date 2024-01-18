05f4eddfa8fffdaa204a3e4c198c0893271e4218
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-18 23:24:29 +0530

    feat:some styling compleated)

---
+++ components/userDetailsCard/UserDetailsCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UserDetailsCard.css";
+
+function UserDetailsCard() {
+  return <div className="userDetailsCardContainer"></div>;
+}
+
+export default UserDetailsCard;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
+import UserDetailsCard from "../components/userDetailsCard/UserDetailsCard";
 
 function App() {
   return (
-    <>
+    <div className="dashboardScreen">
       <SideNavigationBar />
-    </>
+      <UserDetailsCard />
+    </div>
   );
 }
 

afd32b5f1ebc1731054f2eb08f2720377bdc59a4
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-18 22:19:38 +0530

    feat(components/IconButton.jsx):font awsom icon added

--- components/IconButton/IconButton.jsx
+++ components/IconButton/IconButton.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./IconButton.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 function IconButton() {
   return (
     <div className="iconButtonContainer">
-      <div className="iconButtonIconContainer"></div>
+      <div className="iconButtonIconContainer">
+        <FontAwesomeIcon icon={faUser} />
+      </div>
       <div className="iconbuttonTextContiner">
         <p>Home</p>
       </div>

3257fa38d9771853be11da2849d3936827b10255
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-19 20:33:36 +0530

    feat(app.jsx): card width and height seted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
+import UserDetailsCard from "../components/UserDetailsCard/UserDetailsCard";
 
-function App() {
+const App = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const [screenSizePercentage, setScreenSizePercentage] = useState(screenSize);
+
+  const handleResize = () => {
+    setScreenSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="mainScreenContainer">
-      <div className="sidebarNavigationContainer"></div>
-      <div className="sidebarDetailsContainer"></div>
-    </div>
+    <>
+      <div
+        className="sidebarNavigationContainer"
+        style={{
+          width: (screenSize.width * 20) / 100 - 20,
+          height: screenSize.height - 20,
+          padding: 10,
+        }}
+      >
+        <SideNavigationBar />
+      </div>
+      <div
+        className="sidebarNavigationContainer"
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: (screenSize.width * 80) / 100 - 20,
+          height: screenSize.height - 20,
+          padding: 10,
+        }}
+      >
+        <UserDetailsCard />
+      </div>
+    </>
   );
-}
+};
 
 export default App;

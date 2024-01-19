5b0f3dd23fe211d2e0709608418495af6c5085d4
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-19 23:22:10 +0530

    some styles updated

--- components/SideNavigationBar/SideNavigationBar.jsx
+++ components/SideNavigationBar/SideNavigationBar.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./SideNavigationBar.css";
-
+import IconButton from "../IconButton/IconButton";
 function SideNavigationBar() {
   return (
     <div className="sideNavigationBarContainer">
       <div className="sideNavigationBarHeaddingContainer">
         <h1>Dashborard</h1>
       </div>
-      <div className="sideNavigationBarButtonContainer"></div>
+      <div className="sideNavigationBarButtonContainer">
+        <IconButton
+          width={"16vw"}
+          height={50}
+          backgroundColor="#076DFC"
+          borderRadius={10}
+        />
+        <IconButton
+          width={"16vw"}
+          height={50}
+          backgroundColor="#076DFC"
+          borderRadius={10}
+        />
+        <IconButton
+          width={"16vw"}
+          height={50}
+          backgroundColor="#076DFC"
+          borderRadius={10}
+        />
+      </div>
+      <div className="navigationButtonBottomContainer"></div>
     </div>
   );
 }

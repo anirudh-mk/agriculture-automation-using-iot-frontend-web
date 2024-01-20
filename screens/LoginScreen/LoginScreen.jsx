4776fb98e27d8b9c3c9339d4ab00d8c89e930caf
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-20 15:01:25 +0530

    feat(app.jsx): LoginScreen added to app

---
+++ screens/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LoginScreen.css";
+
+function LoginScreen() {
+  return <div className="loginScreenContainer"></div>;
+}
+
+export default LoginScreen;
--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,74 @@
-import React, { useState, useEffect } from "react";
-import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
-import UserDetailsCard from "../components/UserDetailsCard/UserDetailsCard";
-import "./App.css";
+// import React, { useState, useEffect } from "react";
+// import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
+// import UserDetailsCard from "../components/UserDetailsCard/UserDetailsCard";
+// import "./App.css";
 
-const App = () => {
-  const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+// const App = () => {
+//   const [screenSize, setScreenSize] = useState({
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//   });
 
-  const handleResize = () => {
-    setScreenSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  };
+//   const handleResize = () => {
+//     setScreenSize({
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     });
+//   };
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
+//   useEffect(() => {
+//     window.addEventListener("resize", handleResize);
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
 
-  const [screen, setScreen] = useState("");
-  const handleUserClick = () => {
-    setScreen("user");
-  };
-  const handleVegitabelClick = () => {
-    setScreen("vegitable");
-  };
+//   const [screen, setScreen] = useState("");
+//   const handleUserClick = () => {
+//     setScreen("user");
+//   };
+//   const handleVegitabelClick = () => {
+//     setScreen("vegitable");
+//   };
+//   return (
+//     <>
+//       <div
+//         className="sidebarNavigationContainer"
+//         style={{
+//           width: (screenSize.width * 20) / 100 - 20,
+//           height: screenSize.height - 20,
+//         }}
+//       >
+//         <SideNavigationBar
+//           handleUserClick={handleUserClick}
+//           handleVegitabelClick={handleVegitabelClick}
+//         />
+//       </div>
+//       <div
+//         className="sidebarDetailsContainer"
+//         style={{
+//           width: (screenSize.width * 80) / 100 - 20,
+//           height: screenSize.height - 20,
+//         }}
+//       >
+//         {screen === "user" && <UserDetailsCard />}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import LoginScreen from "../screens/LoginScreen/LoginScreen";
+
+function App() {
   return (
     <>
-      <div
-        className="sidebarNavigationContainer"
-        style={{
-          width: (screenSize.width * 20) / 100 - 20,
-          height: screenSize.height - 20,
-        }}
-      >
-        <SideNavigationBar
-          handleUserClick={handleUserClick}
-          handleVegitabelClick={handleVegitabelClick}
-        />
-      </div>
-      <div
-        className="sidebarDetailsContainer"
-        style={{
-          width: (screenSize.width * 80) / 100 - 20,
-          height: screenSize.height - 20,
-        }}
-      >
-        {screen === "user" && <UserDetailsCard />}
-      </div>
+      <LoginScreen />
     </>
   );
-};
+}
 
 export default App;

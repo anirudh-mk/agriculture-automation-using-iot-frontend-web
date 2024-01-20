50674db49d79144d28d284d60efe4e294babf186
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-20 17:34:26 +0530

    feat(LoginScreen): LoginScreen compleated

--- screens/LoginScreen/LoginScreen.jsx
+++ screens/LoginScreen/LoginScreen.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./LoginScreen.css";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+
+import IconButton from "../../components/IconButton/IconButton";
 
 function LoginScreen() {
   return (
     <div className="loginScreenContainer">
-      <div className="loginScreenInputContainer"></div>
+      <div className="loginScreenInputContainer">
+        <div className="loginScreenInputContainerHeaddingContainer">
+          <h1>hello</h1>
+        </div>
+        <div className="loginScreenInputDataContainer">
+          <p className="loginScreenInputLabels">Username</p>
+          <input className="userNameInput" type="text" />
+          <p className="loginScreenInputLabels">Password</p>
+          <input className="password" type="password" />
+          <p className="loginScreenForgotPassword">ForgotPassword?</p>
+          <IconButton
+            width={300}
+            height={50}
+            backgroundColor="#076DFC"
+            borderRadius={10}
+            buttonText="User"
+            icon={faUsers}
+          />
+        </div>
+      </div>
     </div>
   );
 }

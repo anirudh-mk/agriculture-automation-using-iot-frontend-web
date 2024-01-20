f1955a91a2e9f2281e4a4a0276d7522709ff200f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-20 19:14:52 +0530

    feat(LoginScreen):forgot password and login Handdled

--- screens/LoginScreen/LoginScreen.jsx
+++ screens/LoginScreen/LoginScreen.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
 import TextButton from "../../components/TextButton/TextButton";
 
 function LoginScreen() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [forgotPassword, setForgotPassword] = useState(false);
+
+  const handleUserName = (text) => {
+    setUserName(text.target.value);
+  };
+
+  const handlePassword = (text) => {
+    console.log("hello");
+  };
+
+  const handleForgotPassword = () => {
+    setForgotPassword(!forgotPassword);
+  };
+
+  const handleLogin = () => {
+    console.log("login");
+  };
+
   return (
     <div className="loginScreenContainer">
       <div className="loginScreenInputContainer">
-        <div className="loginScreenInputContainerHeaddingContainer">
-          <h1>hello</h1>
-        </div>
-        <div className="loginScreenInputDataContainer">
-          <p className="loginScreenInputLabels">Username</p>
-          <input className="userNameInput" type="text" />
-          <p className="loginScreenInputLabels">Password</p>
-          <input className="password" type="password" />
-          <p className="loginScreenForgotPassword">ForgotPassword?</p>
-          <div className="loginScreenLoginButtonContainer">
-            <TextButton width={300} height={50} buttonText="Login"></TextButton>
+        {forgotPassword ? (
+          <div>
+            forgot password <p onClick={handleForgotPassword}>d</p>
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="loginScreenInputContainerHeaddingContainer">
+              <h1>hello</h1>
+            </div>
+            <div className="loginScreenInputDataContainer">
+              <p className="loginScreenInputLabels">Username</p>
+              <input
+                className="userNameInput"
+                type="text"
+                onChange={handleUserName}
+              />
+              <p className="loginScreenInputLabels">Password</p>
+              <input
+                className="password"
+                type="password"
+                onChange={handlePassword}
+              />
+              <p
+                className="loginScreenForgotPassword"
+                onClick={handleForgotPassword}
+              >
+                ForgotPassword?
+              </p>
+              <div className="loginScreenLoginButtonContainer">
+                <TextButton
+                  width={300}
+                  height={50}
+                  buttonText="Login"
+                  onClick={handleLogin}
+                ></TextButton>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );

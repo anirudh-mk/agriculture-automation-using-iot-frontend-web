548e0e26628667a41876ce3d00dbca6e731ae6df
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-20 14:55:40 +0530

    optim(UserDetailsCard):code optimized

--- components/userDetailsCard/UserDetailsCard.jsx
+++ components/userDetailsCard/UserDetailsCard.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserDetailsCard.css";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 import IconButton from "../IconButton/IconButton";
 
-function UserDetailsCard() {
+function UserDetailsCard({}) {
+  const [create, setCreate] = useState(false);
+  const handelCreate = () => {
+    setCreate(true);
+  };
+
+  const handleClose = () => {
+    setCreate(false);
+  };
+
   return (
     <div className="userDetailsCardContainer">
       <div className="userDetailsCardTopContainer">
@@ -11,9 +21,42 @@ function UserDetailsCard() {
           <h1>User</h1>
         </div>
         <div className="userDetailsCardCreateButtonContainer">
-          <IconButton width={"8vw"} buttonText="create" />
+          <IconButton
+            width={110}
+            height={40}
+            buttonText="create"
+            onClick={handelCreate}
+            icon={faCirclePlus}
+          />
         </div>
       </div>
+      {create && (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "80%",
+              height: "80%",
+              backgroundColor: "#f99",
+              borderRadius: 10,
+            }}
+          >
+            <div
+              style={{ width: 20, height: 20, backgroundColor: "#fff" }}
+              onClick={handleClose}
+            >
+              x
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

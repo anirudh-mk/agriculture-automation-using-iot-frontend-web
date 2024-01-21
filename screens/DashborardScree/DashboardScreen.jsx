fdf2d9e86671c81d4d4418479725e7af7fa8eea5
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-21 11:01:10 +0530

    feat(DashboardScreen.js):SidebarNavigation added

--- screens/DashborardScree/DashboardScreen.jsx
+++ screens/DashborardScree/DashboardScreen.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./DashboardScreen.css";
 
+import SideNavigationBar from "../../components/SideNavigationBar/SideNavigationBar";
+
 function DashboardScreen() {
   return (
     <div className="dashboardScreenContiner">
-      <div className="dashboardScreenSideContainer"></div>
+      <div className="dashboardScreenSideContainer">
+        <SideNavigationBar />
+      </div>
       <div className="dashboardScreenDetailsContainer"></div>
     </div>
   );

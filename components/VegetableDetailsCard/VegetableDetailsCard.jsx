1491d4290ba1bd63375f248d399421ba1b357f3a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-22 17:38:35 +0530

    feat(VegetabledetailsCard)

---
+++ components/VegetableDetailsCard/VegetableDetailsCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./VegetableDetailsCard.css";
+
+function VegetableDetailsCard() {
+  return <div className="vegetableDetailsCardContainer"></div>;
+}
+
+export default VegetableDetailsCard;

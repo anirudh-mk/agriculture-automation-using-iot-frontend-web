dee561972849ccb3fca93d4306d8122be67d9865
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-23 18:48:10 +0530

    feat(UserCreateCard): some new fields added

--- components/UserCreateCard/UserCreateCard.jsx
+++ components/UserCreateCard/UserCreateCard.jsx
@@ -21,19 +21,56 @@ function UserCreateCard({ handleCloseButton }) {
         </div>
       </div>
       <div className="userCreateCardInputsContianer">
-        <p className="userCreateCardInputHeadding">First Name</p>
-        <input type="text" className="userCreateCardInput" />
-        <p className="userCreateCardInputHeadding">Last Name</p>
-        <input type="text" className="userCreateCardInput" />
-        <p className="userCreateCardInputHeadding">Username</p>
-        <input type="text" className="userCreateCardInput" />
-        <p className="userCreateCardInputHeadding">Email</p>
-        <input type="text" className="userCreateCardInput" />
-        <p className="userCreateCardInputHeadding">Password</p>
-        <input type="text" className="userCreateCardInput" />
-        <p className="userCreateCardInputHeadding">Confirm Password</p>
-        <input type="text" className="userCreateCardInput" />
-        <input type="file" name="" id="" />
+        <div className="userCreateCardInputsRowContainer">
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">First Name</p>
+            <input type="text" className="userCreateCardInput" />
+          </div>
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Last Name</p>
+            <input type="text" className="userCreateCardInput" />
+          </div>
+        </div>
+        <div className="userCreateCardInputsRowContainer">
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Email</p>
+            <input type="email" className="userCreateCardInput" />
+          </div>
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">User Name</p>
+            <input type="text" className="userCreateCardInput" />
+          </div>
+        </div>
+        <div className="userCreateCardInputsRowContainer">
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Password</p>
+            <input type="password" className="userCreateCardInput" />
+          </div>
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Confirm Password</p>
+            <input type="password" className="userCreateCardInput" />
+          </div>
+        </div>
+        <div className="userCreateCardInputsRowContainer">
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Phone</p>
+            <input type="phone" className="userCreateCardInput" />
+          </div>
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Location</p>
+            <input type="password" className="userCreateCardInput" />
+          </div>
+        </div>
+        <div className="userCreateCardInputsRowContainer">
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Machine Code</p>
+            <input type="password" className="userCreateCardInput" />
+          </div>
+          <div className="userCreateCardInputsColumnContainer">
+            <p className="userCreateCardInputHeadding">Address</p>
+            <input type="password" className="userCreateCardInput" />
+          </div>
+        </div>
       </div>
       <div className="userCreateCardBottomButtonContainer">
         <div className="userCreateCardCloseButtonContainer">

2fef5709429c37fccfcd5709e8f24889b186a72c
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-24 17:24:00 +0530

    feat(UserCard):compleated

--- components/UserCard/UserCard.jsx
+++ components/UserCard/UserCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./UserCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 function UserCard() {
-  return <div>UserCard</div>;
+  return (
+    <div className="userCardContainer">
+      <p>1</p>
+      <p>Anirudh</p>
+      <p>farm1</p>
+      <p>Kerala</p>
+      <img src="" alt="" className="userCardUserImage" />
+      <FontAwesomeIcon
+        className="userCardContainerMenuIcon"
+        icon={faEllipsisVertical}
+        size="lg"
+      />
+    </div>
+  );
 }
 
 export default UserCard;

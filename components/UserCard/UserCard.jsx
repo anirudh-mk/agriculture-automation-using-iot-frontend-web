a1c315a9b2229d726da6b7efd3cfc381356525fe
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-24 18:46:11 +0530

    optim(UserCard):

--- components/UserCard/UserCard.jsx
+++ components/UserCard/UserCard.jsx
@@ -6,16 +6,28 @@ import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 function UserCard() {
   return (
     <div className="userCardContainer">
-      <p>1</p>
-      <p>Anirudh</p>
-      <p>farm1</p>
-      <p>Kerala</p>
-      <img src="" alt="" className="userCardUserImage" />
-      <FontAwesomeIcon
-        className="userCardContainerMenuIcon"
-        icon={faEllipsisVertical}
-        size="lg"
-      />
+      <div className="userCardContainerSerialNoContainer">
+        <p>1</p>
+      </div>
+      <div className="userCardContainerUserNameContainer">
+        <p>Anirudh</p>
+      </div>
+      <div className="userCardContainerFarmNameContainer">
+        <p>farm1</p>
+      </div>
+      <div className="userCardContainerLocationContainer">
+        <p>Kerala</p>
+      </div>
+      <div className="userCardContainerImageContainer">
+        <img src="" alt="" className="userCardUserImage" />
+      </div>
+      <div className="userCardContainerOptionContainer">
+        <FontAwesomeIcon
+          className="userCardContainerMenuIcon"
+          icon={faEllipsisVertical}
+          size="lg"
+        />
+      </div>
     </div>
   );
 }

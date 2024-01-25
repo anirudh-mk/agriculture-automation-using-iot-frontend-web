cc292adcf576431dbf2ab5d94a7e11e3b410fe4b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-25 22:01:33 +0530

    feat(app.jsx):axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LoginScreen from "../screens/LoginScreen/LoginScreen";
 import DashboardScreen from "../screens/DashborardScree/DashboardScreen";
+import axios from "axios";
 
 function App() {
   const [login, setLogin] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://dev.mulearn.org/api/v1/register/community/list/")
+      .then(function (response) {
+        // handle success
+        console.log(response.data.response.communities);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  });
 
   const handleLogin = () => {
     if (username === "anirudh" && password === "1234") {

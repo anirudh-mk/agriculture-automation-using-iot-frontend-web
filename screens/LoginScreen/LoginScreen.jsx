af83e1a99697c7b87a928d1429d81a462d243ac7
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-25 17:36:25 +0530

    feat(LoginScreen): Login validation added

--- screens/LoginScreen/LoginScreen.jsx
+++ screens/LoginScreen/LoginScreen.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./LoginScreen.css";
 import TextButton from "../../components/TextButton/TextButton";
 import LoginScreenImage from "../../assets/loginScreenImage.png";
-function LoginScreen({ handleLogin }) {
+function LoginScreen({
+  handleLogin,
+  handleUsername,
+  handlePassword,
+  isLoginError,
+}) {
   return (
     <div className="loginScreenContainer">
       <div className="loginScreenCardContianer">
@@ -14,9 +19,20 @@ function LoginScreen({ handleLogin }) {
             <h1 className="loginScreenHeadding">Welcome Back!</h1>
             <p className="loginScreenDescription">Login to continue</p>
             <p>Username</p>
-            <input className="loginScreenInputs" type="text" />
+            <input
+              className="loginScreenInputs"
+              type="text"
+              onChange={handleUsername}
+            />
             <p>Password</p>
-            <input className="loginScreenInputs" type="password" />
+            <input
+              className="loginScreenInputs"
+              type="password"
+              onChange={handlePassword}
+            />
+            {isLoginError && (
+              <p className="error">invalid username or password</p>
+            )}
             <div className="loginScreenButtonContainer">
               <TextButton
                 width={"45%"}
--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,39 @@ import DashboardScreen from "../screens/DashborardScree/DashboardScreen";
 
 function App() {
   const [login, setLogin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
 
   const handleLogin = () => {
-    setLogin(true);
+    if (username === "anirudh" && password === "1234") {
+      setLogin(true);
+    } else {
+      setLoginError(true);
+    }
   };
   const handleLogout = () => {
     setLogin(false);
   };
+
+  const handleUsername = (text) => {
+    setUsername(text.target.value);
+  };
+  const handlePassword = (text) => {
+    setPassword(text.target.value);
+  };
+
   return (
     <>
       {login ? (
         <DashboardScreen handleLogout={handleLogout} />
       ) : (
-        <LoginScreen handleLogin={handleLogin} />
+        <LoginScreen
+          handleLogin={handleLogin}
+          handleUsername={handleUsername}
+          handlePassword={handlePassword}
+          isLoginError={loginError}
+        />
       )}
     </>
   );
